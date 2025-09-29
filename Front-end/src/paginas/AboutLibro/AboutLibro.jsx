f86707ecd47e07c40bbd65bbd369a653e725ae11
Eduardo Guerrero <lalito2003mg@gmail.com>
2025-09-28 23:04:45 -0600

    vista de nosotros distriu¿buida

--- Front-end/src/paginas/AboutLibro/AboutLibro.jsx
+++ Front-end/src/paginas/AboutLibro/AboutLibro.jsx
@@ -19,18 +19,51 @@ function LibroDetalle() {
     <div className="detalle-container">
       <Header />
       <div className="container-detalle-inner">
-        <img src={info.imagen} alt={titulo} className="detalle-img" />
-        <div className="detalle-info">
-          <h1>{titulo}</h1>
-          <p>
-            <strong>Precio:</strong> ${info.precio}
-          </p>
-          <p>
-            <strong>Calificación:</strong> ⭐ {info.calificacion}
-          </p>
-          <p>
-            <strong>Descripción:</strong> {info.descripcion}
-          </p>
+        {/*container superior*/}
+        <div className="container-up">
+          <div className="container-left-up">
+            <div className="detalle-container-imagen">
+              <img src={info.imagen} alt={titulo} className="detalle-img" />
+            </div>
+          </div>
+          <div className="container-right-up">
+            <div className="detalle-container-titulo">
+              <h1>{titulo}</h1>
+            </div>
+
+            <div className="detalle-container-autor">
+              <h1>Autor: {info.autor}</h1>
+            </div>
+          </div>
+        </div>
+
+        {/*container inferior*/}
+        <div className="container-down">
+          <div className="container-left-down">
+            <div className="detalle-container-descripcion">
+              <h1>Descripción:</h1>
+              <h2>{info.reseña}</h2>
+            </div>
+          </div>
+
+          <div className="container-right-down">
+            <div className="detalle-container-precio">
+              <h3>Precio: ${info.precio}.00</h3>
+            </div>
+
+            <div className="detalle-container-rate">
+              <h1>Calificación: </h1><label>{info.calificacion}</label>
+            </div>
+
+            <div className="detalle-container-categoria">
+              <h1>Categorias:</h1>
+              <ul>
+                {Object.entries(info.categoria).map(([key, value], index) => (
+                  <li className="item-categoria" key={index}>{value}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
